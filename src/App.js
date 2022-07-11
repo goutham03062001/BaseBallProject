8a766a9f01b7e44e6e7b15a442725db49c53ed34
savanp08 <savaneddyp08@gmail.com>
2022-07-11 14:17:08 +0530

    aaa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import AddNew from "./AddNew";
+import  LoginPage  from './Pages/LoginPage/LoginPage.js'
+import RegisterPage from './Pages/RegisterPage/Register.js'
+import {Routes , Route
+,BrowserRouter as Router
 
+} from 'react-router-dom'
 import 'antd/dist/antd.css';
+import HomePage from './Pages/HomePage/HomePage.js';
+import UserSignUp from './Pages/TestLogin/UserSignUp.js';
+import UserLogin from './Pages/TestLogin/UserLogin.js';
+import AdminSignUp from './Pages/TestLogin/AdminSignUp.js';
+import AdminLogin from './Pages/TestLogin/AdminLogin.js';
+
 function App() {
- 
   return (
     <>
-     <AddNew/> 
+    
+    <Routes>
+      
+      <Route exact path="/register" element={<RegisterPage/>}/>
+      <Route exact path ="/login" element = {<LoginPage/>}/>
+      <Route exact path ="/Register2" element = {<UserSignUp/>}/>
+      <Route exact path ="/Login2" element = {<UserLogin/>}/>
+      <Route exact path ="/Register2" element = {<AdminSignUp/>}/>
+      <Route exact path ="/Admin/Login2" element = {<AdminLogin/>}/>
+      <Route exact path="/Account/:UserName" element={<HomePage/>} />
+      
+      <Route  path = "/" element = {<HomePage/>}/>
+
+    </Routes>
+    
     </>
   )
 }
