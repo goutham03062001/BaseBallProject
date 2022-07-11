315d8de61fd24ee02a6310f98bfb1a95935e6d4b
Goutham0306 <goutham.190422@gmail.com>
2022-07-11 21:54:51 +0530

    adding more button

--- src/Pages/HomePage/EventCard.js
+++ src/Pages/HomePage/EventCard.js
@@ -1,15 +1,19 @@
-import { Card , Button } from "antd";
+import { Card , Button ,Modal} from "antd";
 import React from "react";
 import './EventCard.css';
 
 const EventCard = (props) => {
   const event = props.event;
   const key = props.index;
+  const[visible,setVisible] = useState(false);
+  const showModel=()=>{
+    setVisible(true);
+  }
   return (
     <Card key={key} style={{ width: 300 , marginBottom: 16 }}
       
     >
-      <div>
+      <div onClick={showModel}>
         <h2>
           {event.team1} vs {event.team2}
         </h2>
@@ -29,6 +33,18 @@ const EventCard = (props) => {
           >Delete</Button>
         </div>
       </div>
+      <Modal visible={visible}>
+        <h3>{event.team1} vs {event.team2}</h3>
+        <br/>
+        <p>{event.venue}</p>
+        <p>{event.date}</p>
+        <p>{event.time}</p>
+        <p>{event.result}</p>
+        <p>{event.refree}</p>
+        
+        <p>{event.description}</p>
+        <Button onClick={e=>setVisible(false)}>Close Me</Button>
+      </Modal>
     </Card>
   );
 };
