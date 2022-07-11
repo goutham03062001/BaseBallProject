3e14d60a8d160736fc3b6c30ee9abc032515ca43
Rogue-striker <jyothikiran52@gmail.com>
2022-07-11 13:04:34 +0530

    added homepage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import  LoginPage  from './Pages/LoginPage/LoginPage.js'
 import RegisterPage from './Pages/RegisterPage/Register.js'
 import {Routes , Route} from 'react-router-dom'
 import 'antd/dist/antd.css';
+import HomePage from './Pages/HomePage/HomePage.js';
 
 function App() {
- 
   return (
     <>
     <Routes>
+      <Route  path = "/" element = {<HomePage/>}/>
       <Route path="/register" element={<RegisterPage/>}/>
       <Route path ="/login" element = {<LoginPage/>}/>
     </Routes>
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import EventCard from "./EventCard";
+import './Homepage.css'
+
+const Events = [
+  {
+    team1: "team1",
+    team2: "team2",
+    venue: "venue",
+    date: "date",
+    time: "time",
+    result: "result",
+  },
+  {
+    team1: "team1",
+    team2: "team2",
+    venue: "venue",
+    date: "date",
+    time: "time",
+    result: "result",
+  },
+  {
+    team1: "team1",
+    team2: "team2",
+    venue: "venue",
+    date: "date",
+    time: "time",
+    result: "result",
+  },
+];
 
 const HomePage = () => {
   return (
-    <div>
-        <h1>Home Page</h1>
+    <div className="tournaments">
+      <h1>Tournaments</h1>
+      <div className="tournaments_list">
+        {Events.map((event, index) => {
+          return <EventCard key={index} event={event} />;
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
